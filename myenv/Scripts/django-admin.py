a65ff07d28fdcb1547d6c21e52ff1fb752453249
rishii <rishii8ons@gmail.com>
2021-05-18 14:03:45 +0900

    My Django Girls app, first commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\rishii\sourcep\workspaces\djangogirls\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
